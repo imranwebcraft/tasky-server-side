9217a42bc4808021643cfa5295822c0a92ba254f
imranwebcraft <imranhossainsakil22@gmail.com>
2023-12-22 05:44:41 +0600

    handle error in the server and add folder structure

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 
 // App variable
 const app = express();
-app.use(cors());
+app.use(
+	cors({
+		origin: ['http://localhost:5173'],
+	})
+);
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -17,10 +21,24 @@ app.listen(port, () => {
 	console.log(`Tasky Server is running on port ${port}`);
 });
 
+// handling all (get,post,update,delete.....) unhandled routes
+app.all('*', (req, res, next) => {
+	const error = new Error(`Can't find ${req.originalUrl} on the server`);
+	error.status = 404;
+	next(error);
+});
+
+app.use((err, _req, res, _next) => {
+	res.status(err.status || 500).json({
+		message: err.message,
+		errors: err.errors,
+	});
+});
+
 // Connect DB
 
 mongoose
-	.connect(process.env.DB_LOCATION)
+	.connect(process.env.DB_LOCATION, { dbName: process.env.DB_NAME })
 	.then(() => {
 		console.log('DB Connected');
 	})
